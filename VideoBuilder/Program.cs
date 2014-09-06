40f8fc527e7ddae218d610462c00e54b60aac526
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-06 23:34:06 +0300

    minor updates

--- VideoBuilder/Program.cs
+++ VideoBuilder/Program.cs
@@ -8,20 +8,21 @@ using AForge.Video.FFMPEG;
 
 namespace VideoBuilder {
     class Program {
+        private const string ImgExtentsion = ".png";
         static void Main(string[] args){
             var paths = args[0].Split(';');
             foreach (var path in paths.Where(s => !string.IsNullOrEmpty(s))){
                 
-                var files = Directory.GetFiles(path, "*.bmp");
-                var images = GetImages(files).GroupBy(s => Regex.Replace(s, @"([^\d]*)([\d]*)([^\d]*)", "$1$3", RegexOptions.Singleline | RegexOptions.IgnoreCase));
+                var files = Directory.GetFiles(path, "*.png");
+                var images = GetImages(files.ToArray()).GroupBy(s => Regex.Replace(s, @"([^\d]*)([\d]*)([^\d]*)", "$1$3", RegexOptions.Singleline | RegexOptions.IgnoreCase));
                 foreach (var grouping in images){
                     Console.WriteLine("Creating Videos for " + path);
                     var videoFileName = Path.Combine(path + "", grouping.Key + ".avi");
                     if (File.Exists(videoFileName))
                         File.Delete(videoFileName);
                     var videoFileWriter = new VideoFileWriter();
-                    int frameRate = 4;
-                    using (var bitmap = new Bitmap(Path.Combine(path, grouping.First()+".bmp"))){
+                    const int frameRate = 4;
+                    using (var bitmap = new Bitmap(Path.Combine(path, grouping.First()+ImgExtentsion))){
                         videoFileWriter.Open(videoFileName, bitmap.Width, bitmap.Height, frameRate,VideoCodec.MPEG4);
                     }
                     WriteFranes(grouping, path, videoFileWriter, frameRate);
@@ -36,7 +37,7 @@ namespace VideoBuilder {
 
         private static void WriteFranes(IEnumerable<string> grouping, string path, VideoFileWriter videoFileWriter, int frameRate){
             int index = 0;
-            var images = grouping.Select(s => Path.Combine(path, s + ".bmp")).ToArray();
+            var images = grouping.Select(s => Path.Combine(path, s + ImgExtentsion)).ToArray();
             foreach (var image in images){
                 index++;
                 using (var bitmap = new Bitmap(image)){
@@ -44,10 +45,10 @@ namespace VideoBuilder {
                     videoFileWriter.WriteVideoFrame(bitmap, timeSpan);
                 }
             }
-            Console.WriteLine("Found "+grouping.Count()+" images");
+            Console.WriteLine("Found " + images.Count() + " images");
         }
 
-        private static IEnumerable<string> GetImages(IEnumerable<string> files){
+        private static IEnumerable<string> GetImages(string[] files){
             var list = new List<string>();
             list.AddRange(GetImagesCore(files, s => !s.Contains(".")));
             foreach (var platform in new []{"win","web"}){
