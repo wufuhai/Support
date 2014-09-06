131ede4c63ace7b4164712f71545f5691aeb997b
apobekiaris <apostolis.bekiaris@gmail.com>
2014-09-07 01:42:17 +0300

    hide proccessAsUser window because it is recorded

--- ProcessAsUser/ProcessAsUserWrapper/Program.cs
+++ ProcessAsUser/ProcessAsUserWrapper/Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace ProcessAsUserWrapper {
     static class Program {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +24,8 @@ namespace ProcessAsUserWrapper {
             mainForm.Load+= (sender, eventArgs) =>{
                 var processStartInfo = new ProcessStartInfo(args[0], args.Length == 2 ? args[1] : null) {UseShellExecute=false, RedirectStandardOutput = true,CreateNoWindow = true};
                 var process = Process.Start(processStartInfo);
-                
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, SW_HIDE);
                 Debug.Assert(process != null, "process != null");
                 process.WaitForExit();
                 var streamWriter = File.CreateText("processAsuserWrapper.log");
